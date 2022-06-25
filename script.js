357e577fdf034fa8e66c29915a5e565cc8bb80aa
Ibrahim I <ibrahim@Ibrahims-MacBook-Pro.local>
2022-06-25 12:41:20 -0700

    Button Clicker Assignment

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function changeBtn(text){
+    console.log("this button is working")
+    text.innerText = "Logout"
+}
+
+function removeBtn(element){
+    console.log ("this is working")
+    element.remove()
+}
+
+function likeBtns(likes) {
+    console.log("this is working")
+    alert("Ninja was liked")
+}
